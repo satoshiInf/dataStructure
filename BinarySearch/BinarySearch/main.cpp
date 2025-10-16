49654b2ad969ae1334ce9322fb3d4f1872c9a420
satoshi_new <satoshi_new@satoshi-news-MacBook-Pro.local>
2025-10-17 05:45:21 +0700

    first commit

---
+++ BinarySearch/BinarySearch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BinarySearch
+//
+//  Created by satoshi_new on 2025/10/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
